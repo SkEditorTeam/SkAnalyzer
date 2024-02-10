c6acd675622e806f091b3868224fceddf2a91277
GliczDev <67753196+GliczDev@users.noreply.github.com>
2024-02-10 23:40:05 +0100

    Bump MockBukkit

--- api/src/main/java/me/glicz/skanalyzer/mockbukkit/AnalyzerUnsafeValues.java
+++ api/src/main/java/me/glicz/skanalyzer/mockbukkit/AnalyzerUnsafeValues.java
@@ -1,12 +1,22 @@
 package me.glicz.skanalyzer.mockbukkit;
 
 import be.seeseemelk.mockbukkit.MockUnsafeValues;
+import io.papermc.paper.plugin.lifecycle.event.LifecycleEventManager;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
-@SuppressWarnings("deprecation")
+import java.util.function.BooleanSupplier;
+
+@SuppressWarnings({"deprecation", "UnstableApiUsage"})
 public class AnalyzerUnsafeValues extends MockUnsafeValues {
     @Override
     public ItemStack modifyItemStack(ItemStack stack, String arguments) {
         return stack;
+    }
+
+    @Override
+    public LifecycleEventManager<Plugin> createPluginLifecycleEventManager(JavaPlugin javaPlugin, BooleanSupplier booleanSupplier) {
+        return null;
     }
 }
