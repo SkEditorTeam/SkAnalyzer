eabc1cf546e7229676ebc0b7324ab04dc0950a9a
GliczDev <67753196+GliczDev@users.noreply.github.com>
2024-05-30 19:21:05 +0200

    Update MockBukkit to v3.88.1

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 dependencies {
     compileOnly("org.projectlombok:lombok:1.18.32")
     annotationProcessor("org.projectlombok:lombok:1.18.32")
-    api("com.github.MockBukkit:MockBukkit:PR1018-SNAPSHOT")
+    api("com.github.MockBukkit:MockBukkit:v3.88.1")
     implementation("org.apache.logging.log4j:log4j-core:3.0.0-alpha1")
     implementation("org.slf4j:slf4j-simple:2.0.9")
     implementation("commons-io:commons-io:2.14.0")
