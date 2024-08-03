f01a58e340cc00d4709e345ead6050aed86264d5
GliczDev <67753196+GliczDev@users.noreply.github.com>
2024-08-03 22:28:14 +0200

    Better logging using logback

--- api/src/main/java/me/glicz/skanalyzer/LoggerType.java
+++ api/src/main/java/me/glicz/skanalyzer/LoggerType.java
@@ -1,16 +1,37 @@
 package me.glicz.skanalyzer;
 
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.apache.logging.log4j.Level;
+import org.slf4j.LoggerFactory;
+
+import java.net.URL;
 
 @Getter
 @AllArgsConstructor
 public enum LoggerType {
-    DISABLED(null, Level.OFF),
-    NORMAL("SkAnalyzer", Level.ALL),
-    PLAIN("PlainLogger", Level.ALL);
+    NORMAL("/"),
+    PLAIN("/plain-logback.xml"),
+    DISABLED("/disabled-logback.xml");
 
-    private final String loggerName;
-    private final Level loggerLevel;
+    private final String configFile;
+
+    public void loadConfiguration() {
+        URL config = getClass().getResource(configFile);
+        if (config == null) return;
+
+        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+        loggerContext.reset();
+
+        JoranConfigurator configurator = new JoranConfigurator();
+        configurator.setContext(loggerContext);
+
+        try {
+            configurator.doConfigure(config);
+        } catch (JoranException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
