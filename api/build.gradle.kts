19ce0c0755b2c2b97529aa7946a1701181be3f9f
GliczDev <67753196+GliczDev@users.noreply.github.com>
2024-08-03 22:28:14 +0200

    Better logging using logback

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -6,9 +6,13 @@ plugins {
 dependencies {
     compileOnly("org.projectlombok:lombok:1.18.32")
     annotationProcessor("org.projectlombok:lombok:1.18.32")
-    api("org.mockbukkit.mockbukkit:mockbukkit-v1.21:4.20.1")
-    api("org.apache.logging.log4j:log4j-core:3.0.0-alpha1")
-    api("org.slf4j:slf4j-simple:2.0.9")
+    api("org.mockbukkit.mockbukkit:mockbukkit-v1.21:4.20.1") {
+        exclude("org.apache.logging.log4j")
+        exclude("org.slf4j")
+    }
+    api("org.apache.logging.log4j:log4j-to-slf4j:3.0.0-beta2")
+    api("ch.qos.logback:logback-classic:1.5.6")
+    api("org.slf4j:jul-to-slf4j:2.0.13")
     api("commons-io:commons-io:2.14.0")
     api("commons-lang:commons-lang:2.6")
 }
