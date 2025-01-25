eec6780766168e32cf9501c4d9398279eb78d3a0
GliczDev <67753196+GliczDev@users.noreply.github.com>
2025-01-25 12:28:34 +0100

    Mark plugin package as internal

--- api/src/main/java/me/glicz/skanalyzer/plugin/rewriter/call/method/package-info.java
+++ api/src/main/java/me/glicz/skanalyzer/plugin/rewriter/call/method/package-info.java
@@ -1,4 +1,6 @@
+@ApiStatus.Internal
 @NullMarked
 package me.glicz.skanalyzer.plugin.rewriter.call.method;
 
+import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
--- api/src/main/java/me/glicz/skanalyzer/plugin/rewriter/package-info.java
+++ api/src/main/java/me/glicz/skanalyzer/plugin/rewriter/package-info.java
@@ -1,4 +1,6 @@
+@ApiStatus.Internal
 @NullMarked
 package me.glicz.skanalyzer.plugin.rewriter;
 
+import org.jetbrains.annotations.ApiStatus;
 import org.jspecify.annotations.NullMarked;
