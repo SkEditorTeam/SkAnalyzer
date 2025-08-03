e6022b0a1560d3e6bf946cd58d3fe65a6e351484
GliczDev <67753196+GliczDev@users.noreply.github.com>
2025-08-03 09:24:07 +0200

    Fix app main class throwing on unrecognized command line options

--- app/src/main/java/me/glicz/skanalyzer/app/SkAnalyzerApp.java
+++ app/src/main/java/me/glicz/skanalyzer/app/SkAnalyzerApp.java
@@ -46,6 +46,8 @@ public class SkAnalyzerApp {
                 .withRequiredArg()
                 .ofType(File.class);
 
+        optionParser.allowsUnrecognizedOptions();
+
         OptionSet optionSet = optionParser.parse(args);
 
         this.skAnalyzer = SkAnalyzer.builder()
