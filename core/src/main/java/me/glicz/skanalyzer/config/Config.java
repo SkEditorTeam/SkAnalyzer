2fffba5522d7d6c52985c28aba50aec9a730e303
GliczDev <67753196+GliczDev@users.noreply.github.com>
2025-10-15 21:30:29 +0200

    Add Config#allowAutoReload for Skript 2.13

--- core/src/main/java/me/glicz/skanalyzer/config/Config.java
+++ core/src/main/java/me/glicz/skanalyzer/config/Config.java
@@ -4,6 +4,7 @@ import org.spongepowered.configurate.objectmapping.ConfigSerializable;
 
 @ConfigSerializable
 public final class Config {
+    public boolean allowAutoReload;
     public ForcedHooks forcedHooks = new ForcedHooks();
 
     @ConfigSerializable
