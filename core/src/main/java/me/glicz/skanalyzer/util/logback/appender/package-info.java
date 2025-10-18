63a7a349df7f6566c17eafdf2a4d6194606fe3c8
GliczDev <67753196+GliczDev@users.noreply.github.com>
2025-10-18 02:00:52 +0200

    Simplify logback config

--- core/src/main/java/me/glicz/skanalyzer/util/logback/appender/package-info.java
+++
@@ -1,6 +0,0 @@
-@ApiStatus.Internal
-@NullMarked
-package me.glicz.skanalyzer.util.logback.appender;
-
-import org.jetbrains.annotations.ApiStatus;
-import org.jspecify.annotations.NullMarked;
--- core/src/main/java/me/glicz/skanalyzer/util/logback/converter/package-info.java
+++
@@ -1,6 +0,0 @@
-@ApiStatus.Internal
-@NullMarked
-package me.glicz.skanalyzer.util.logback.converter;
-
-import org.jetbrains.annotations.ApiStatus;
-import org.jspecify.annotations.NullMarked;
