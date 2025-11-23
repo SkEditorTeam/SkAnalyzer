9c20b99e078f75a730e24141ee46c0ce88ab1747
GliczDev <67753196+GliczDev@users.noreply.github.com>
2025-11-23 22:50:05 +0100

    Mark process tasks as untracked

--- build-logic/bootstrap-plugin/src/main/kotlin/me/glicz/skanalyzer/plugin/task/ProcessLibraries.kt
+++ build-logic/bootstrap-plugin/src/main/kotlin/me/glicz/skanalyzer/plugin/task/ProcessLibraries.kt
@@ -1,7 +1,9 @@
 package me.glicz.skanalyzer.plugin.task
 
 import me.glicz.skanalyzer.plugin.bootstrap.runtimeLibraries
+import org.gradle.api.tasks.UntrackedTask
 
+@UntrackedTask(because = "process task")
 abstract class ProcessLibraries : AbstractProcessTask() {
     override val assets
         get() = runtimeLibraries(project)
--- build-logic/bootstrap-plugin/src/main/kotlin/me/glicz/skanalyzer/plugin/task/ProcessPlugins.kt
+++ build-logic/bootstrap-plugin/src/main/kotlin/me/glicz/skanalyzer/plugin/task/ProcessPlugins.kt
@@ -1,7 +1,9 @@
 package me.glicz.skanalyzer.plugin.task
 
 import me.glicz.skanalyzer.plugin.bootstrap.plugins
+import org.gradle.api.tasks.UntrackedTask
 
+@UntrackedTask(because = "process plugins")
 abstract class ProcessPlugins : AbstractProcessTask() {
     override val assets
         get() = plugins(project)
